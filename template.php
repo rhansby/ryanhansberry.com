657c0d1c884a429a3e435d73c263d0fbf568eb0b
Ryan Hansberry <rhansby@gmail.com>
2013-09-03 21:21:39 -0700

    fix bad jquery link

--- template.php
+++ template.php
@@ -101,7 +101,7 @@ function print_footer($additional_js = '')
                 reach me at <a href="mailto:rhansby@gmail.com">rhansby@gmail.com</a>
             </footer>
 
-            <!script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
             <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 
             <script src="/js/plugins.js"></script>
